602e91f63b4e265d803571b25f9b130b51fb1149
maddenlockin <maddenlockin@gmail.com>
2021-11-01 17:02:10 -0700

    colors randomly changing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import RandomizeColor from '../../containers/RandomizeColor';
 
 export default function App() {
-  return <RandomizeColor/>;
+  return <RandomizeColor />;
 }
--- src/containers/RandomizeColor.jsx
+++ src/containers/RandomizeColor.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import ColorDisplay from '../components/random-color/colorDisplay';
 
-export default class RandomColor extends Component {
+export default class RandomizeColor extends Component {
     state = {
-        color: '',
+        color: '#000000',
     };
 
-    handleRandomColor = (color) => {
-        this.setState({ currentColor: color });
+    createRandomHex() {
+        const newHex = () => Math.floor(Math.random() * 256).toString(16);
+
+        return `#${newHex()}${newHex()}${newHex()}`.padEnd(7, '0')
     }
+
+    handleRandomColor = () => {
+        const newColor = this.createRandomHex();
+
+        this.setState(({ color }) => ({
+            color: newColor
+        }));
+
+        console.log(this.state)
+    }
+
+    componentDidMount() {
+        setInterval(this.handleRandomColor.bind(this), 1000);
+    }
+
     render() {
         return (
-            <></>
+            <>
+                <ColorDisplay
+                    color={this.state.color}
+                />
+            </>
         )
     }
 }
